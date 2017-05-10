3f698a7dd28f238d7928392e487b78dc60f5269a
TTCAD\r-takahashi <r-takahashi@ttc-net.co.jp>
2017-05-10 14:51:18 +0900

    変数を使って画像と画像番号を変化させる

--- viewer.js
+++ viewer.js
@@ -1,11 +1,15 @@
+var pictNo = 1;
+
 function revBtnClick(){
-  document.getElementById('no').innerHTML = 'No.1';
-  document.getElementById('pict').src = 'img/1.jpg';
+  pictNo--;
+  document.getElementById('no').innerHTML = 'No.' + pictNo;
+  document.getElementById('pict').src = 'img/' + pictNo + '.jpg';
   document.getElementById('no').className = 'no1';
 }
 
 function fwdBtnClick(){
-  document.getElementById('no').innerHTML = 'No.2';
-  document.getElementById('pict').src = 'img/2.jpg';
+  pictNo++;
+  document.getElementById('no').innerHTML = 'No.' + pictNo;
+  document.getElementById('pict').src = 'img/'+ pictNo +'.jpg';
   document.getElementById('no').className = 'no2';
 }
